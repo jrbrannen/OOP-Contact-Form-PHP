7afcc3ca768bc17b60e7157965ea379f998519dd
jrbrannen <71732832+jrbrannen@users.noreply.github.com>
2022-02-09 14:28:02 -0500

    Initial Commit

---
+++ week04/inc/ContactForm.php
@@ -0,0 +1,234 @@
+<?php
+
+class ContactForm {
+
+    var $validform = true;
+    var $errorArray = array();
+    var $firstName;
+    var $lastName;
+    var $dateOfBirth;
+    var $emailAddress;
+    var $middleName;
+    var $message = "";
+
+    public function __construct()
+    {
+        
+    }
+
+    public function getValidform(){
+        return $this->validform;
+    }
+
+    public function setValidform($validform){
+        $this->validform = $validform;
+        return $this;
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+
+    public function setFirstName($firstName){
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName){
+        $this->lastName = $lastName;
+        return $this;
+    }
+  
+    public function getDateOfBirth(){
+        return $this->dateOfBirth;
+    }
+
+    public function setDateOfBirth($dateOfBirth){
+        $this->dateOfBirth = $dateOfBirth;
+        return $this;
+    }
+
+    public function getEmailAddress(){
+        return $this->emailAddress;
+    }
+
+    public function setEmailAddress($emailAddress){
+        $this->emailAddress = $emailAddress;
+        return $this;
+    }
+
+    public function getMiddleName(){
+        return $this->middleName;
+    }
+
+    public function setMiddleName($middleName){
+        $this->middleName = $middleName;
+        return $this;
+    }
+ 
+    public function getMessage(){
+        return $this->message;
+    }
+
+    public function setMessage($message){
+        $this->message = $message;
+        return $this;
+    }
+
+    /**
+     * Sends an email response using key:value pairs from the contact form
+     */
+    function sendReponse($inFirstName, $inLastName, $inDateOfBirth, $inEmailAddress, $inMessage){
+        $to = $inEmailAddress;        // email address
+        $subject = "Contact Form Confirmation message";     // subject
+        $message =  "<br>Hello,<br> 
+                    This is a confirmation message that my contact form processed. 
+                    All the contact form information is as follows:" . 
+                    "<br>First Name: " . $inFirstName .
+                    "<br>Last Name: " . $inLastName .
+                    "<br>Date Of Birth: " . $inDateOfBirth .
+                    "<br>Email Address: " . $to . 
+                    "<br>Message: " . $inMessage .
+                    "<br>Thank you.  
+                    <br><br>Regards, 
+                    <br> Jeremy Brannen";
+        
+        $headers = "From: jeremybrannen@jeremybrannen.info" . "\r\n";   // email address from host server
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";    // formats for http
+
+        mail($to,$subject,$message,$headers);   // send out email
+    }
+
+    /**
+     * Sanitizes string input, checks for empty value or string value
+     * over 25 chars and sets appropiate error message.  Returns false if
+     * string is not valid. Returns true if string is valid. 
+     */
+    function validateFirstName($inName){
+        
+        define("MAX_FNAME_LENGTH", 15);
+        $inName = filter_var($inName, FILTER_SANITIZE_STRING);
+        $inName = htmlspecialchars($inName, ENT_NOQUOTES);
+        
+        if($inName == ""){
+            $this->errorArray[] = ("First name is required");
+            $this->validform = false;
+            return;
+        }
+        if(strlen($inName) > MAX_FNAME_LENGTH){
+            $this->errorArray[] = ("First name cannot be more than 15 characters");
+            $this->validform = false;
+            return;
+        }
+    }
+
+    /**
+     * Sanitizes string input, checks for empty value or string value
+     * over 15 chars and sets appropiate error message.  Returns false if
+     * string is not valid. Returns true if string is valid. 
+     */
+    function validateLastName($inName){
+        define("MAX_LNAME_LENGTH", 20);
+        $inName = filter_var($inName, FILTER_SANITIZE_STRING);
+        $inName = htmlspecialchars($inName, ENT_NOQUOTES);
+        
+        if($inName == ""){
+            $this->errorArray[] = ("Last name is required");
+            $this->validform = false;
+            return;
+        }
+        if(strlen($inName) > MAX_LNAME_LENGTH){
+            $this->errorArray[] = ("Last name cannot be more than 20 characters");
+            $this->validform = false;
+            return;
+        }
+    }
+
+    /**
+     * Checks date input variable for mm/dd/yyyy pattern and sets appropiate
+     * error message.  Returns true if format is valid.  Returns false if 
+     * format is not valid.
+     */
+    function validateDateOfBirth($inDate){
+        define("MIN_MONTH", 1);
+        define("MAX_MONTH", 12);
+        define("MIN_DAY", 1);
+        define("MAX_DAY", 31);
+        define("MIN_YEAR", 1910);
+        define("MAX_YEAR", 2021);
+        $dateRegex = "/^\d{2}\/\d{2}\/\d{4}$/";
+
+        if($inDate == ""){
+            $this->errorArray[] = ("Date of birth is required");
+            $this->validform = false;
+            return;
+        }
+        if(preg_match($dateRegex, $inDate) === 0){
+            $this->errorArray[] = ("Date must be in mm/dd/yyyy format");
+            $this->validform = false;
+            return;
+        }
+        if(substr($inDate, 0, 2) < MIN_MONTH || substr($inDate, 0, 2) > MAX_MONTH){
+            $this->errorArray[] = ("Month must be between 1-12");
+            $this->validform = false;
+            return;
+        }
+        if(substr($inDate, 3, 2) < MIN_DAY || substr($inDate, 3, 2) > MAX_DAY){
+            $this->errorArray[] = ("Day must be between 1-31");
+            $this->validform = false;
+            return;
+        }
+        if(substr($inDate, 6, 4) < MIN_YEAR || substr($inDate, 6, 4) > MAX_YEAR){
+            $this->errorArray[] = ("Year must be between 1910-2021");
+            $this->validform = false;
+            return;
+        }
+    }
+
+    /**
+     * Validates email and sets correct error message. If email is 
+     * valid returns true.  If not valid returns false.
+     */
+    function validateEmail($inEmail){
+        $inEmail = filter_var($inEmail, FILTER_SANITIZE_EMAIL);
+        
+        if($inEmail == ""){
+            $this->errorArray[] = ("Email is required");
+            $this->validform = false;
+            return;
+        }
+        if(filter_var($inEmail, FILTER_VALIDATE_EMAIL) === false){
+            $this->errorArray[] = ("Email is invalid.  Please enter a valid email");
+            $this->validform = false;
+            return;
+        }
+    }
+
+    /**
+     * Sanitizes string input, checks for empty value or string value
+     * over 150 chars and sets appropiate error message.  Returns false if
+     * string is not valid. Returns true if string is valid. 
+     */
+    function validateMessage($inMessage){
+        define("MAX_MESSAGE_LENGTH", 150);
+        $inMessage = filter_var($inMessage, FILTER_SANITIZE_STRING);
+        $inMessage = htmlspecialchars($inMessage, ENT_NOQUOTES);
+
+        if($inMessage == ""){
+            $this->errorArray[] = ("Message is required");
+            $this->validform = false;
+            return;
+        }
+        if(strlen($inMessage) > MAX_MESSAGE_LENGTH){
+            $this->errorArray[] = ("Message length must be 150 characters or less");
+            $this->validform = false;
+            return;
+        }
+    }
+}
+
+?>
---
+++ week04/public/index.php
@@ -0,0 +1,152 @@
+<?php
+    // include Contact form class file
+    include_once "../inc/ContactForm.php";
+
+    $dataArray = $_POST;
+    $successMessage = null;
+
+    // if form is submitted
+    if(isset($dataArray['submit'])){
+
+        // honeypot validation
+        $middleName = $dataArray['middleName'];
+        
+        // if $middleName is not empty refresh the page, else assign contact form properties 
+        // and validate them
+        if(!empty($middleName)){
+            header("refresh:0");
+        }else{
+            // create a new contact form object
+            $contactForm = new ContactForm();
+            
+            // assign $dataArray elements to the contact form object
+            $contactForm->setFirstName($dataArray['firstName']);
+            $contactForm->setLastName($dataArray['lastName']);
+            $contactForm->setDateOfBirth($dataArray['dateOfBirth']);
+            $contactForm->setEmailAddress($dataArray['emailAddress']);
+            $contactForm->setMessage($dataArray['message']);
+
+            /*  validate all input text fields ($dataArray element values).
+                Validform property is set to true.  Validation function calls 
+                will set property to false if a validation fails. If a field 
+                or fields are not valid error message will be stored in an 
+                errorArray and displayed to the view.
+            */
+            $contactForm->validform = true;
+            $contactForm->validateFirstName($contactForm->getFirstName());
+            $contactForm->validateLastName($contactForm->getLastName());
+            $contactForm->validateDateOfBirth($contactForm->getDateOfBirth());
+            $contactForm->validateEmail($contactForm->getEmailAddress());
+            $contactForm->validateMessage($contactForm->getMessage());
+
+            // if the form is valid an email response will be sent, success message will display
+            // in the view, and contact form properties will be reset
+            if($contactForm->validform === true){
+                $contactForm->sendReponse($contactForm->firstName, $contactForm->lastName, $contactForm->dateOfBirth, $contactForm->emailAddress, $contactForm->message);
+                $successMessage = "Form was submitted.  You will recieve an email soon";
+                $contactForm->firstName = "";
+                $contactForm->lastName = "";
+                $contactForm->dateOfBirth = "";
+                $contactForm->emailAddress = "";
+                $contactForm->message = "";
+            }   
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Week 4 Assignment</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatable" content="IE=edge">
+        <meta name="description" content="Week One Assignment" keywords="Week two assignment">
+        <meta name="Author" content="Jeremy Brannen">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        
+        <!--Jeremy Brannen WDV441 Week 4 assignment-->
+        
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        
+        <script>
+        </script>
+        
+        <style> 
+            body{
+                color: #484c9b;
+                background-color: cornsilk;
+            }
+            form div:nth-child(4){
+                display: none;
+            }
+        </style>
+
+    </head>
+
+    <body class="container-fluid">
+
+        <h1 class="text-center"> Week 4 Assignment</h1>
+
+        <h2 class="text-center">Contact Form</h2>
+       
+        <div class="col-md-8 mx-auto">
+            <form class="" id="contactForm" name="contactForm" method="post" action="index.php">
+                <div class="text-center text-primary">
+                    <?php if (!is_null($successMessage)) {?>
+                        <b><?= $successMessage ?></b>
+                    <?php } ?>
+                </div>
+                <div class="text-left text-danger">
+                    <?php if(isset($dataArray['submit']) && count($contactForm->errorArray) > 0){ ?>
+                        <ul>
+                            <?php foreach ($contactForm->errorArray as $errorMessage){ ?>
+                                <li><?= $errorMessage ?></li>
+                            <?php } ?>
+                        </ul></br>
+                    <?php } ?>
+                </div>
+                <div class="form-group">
+                    <label for="firstName">First Name: </label>
+                    <input type="text" class="form-control form-control-sm" name="firstName" id="firstName" value="<?= isset($contactForm->firstName) ? $contactForm->firstName : ""; ?>">
+                </div>
+
+                <div class="form-group">
+                    <?php //honeypot ?>
+                    <label for="middleName">Middle Name: </label>
+                    <input type="text" class="form-control form-control-sm" name="middleName" id="middleName" value="">
+                </div>
+
+                <div class="form-group">
+                    <label for="lastName">Last Name: </label>
+                    <input type="text" class="form-control form-control-sm" name="lastName" id="lastName" value="<?= isset($contactForm->lastName) ? $contactForm->lastName : ""; ?>">
+                </div>
+
+                <div class="form-group">
+                    <label for="dateOfBirth">Date Of Birth: </label>
+                    <input type="text" class="form-control form-control-sm" name="dateOfBirth" id="dateOfBirth" value="<?= isset($contactForm->dateOfBirth) ? $contactForm->dateOfBirth : ""; ?>" placeholder="mm/dd/yyyy">
+                </div>
+
+                <div class="form-group">
+                    <label for="emailAddress">Email: </label>
+                    <input type="text" class="form-control form-control-sm" name="emailAddress" id="emailAddress" value="<?= isset($contactForm->emailAddress) ? $contactForm->emailAddress : ""; ?>" placeholder="youremail@domain.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="message">Message: </label>
+                    <textarea class="form-control" name="message" id="message" value="<?= isset($contactForm->message) ? $contactForm->message : ""; ?>"><?= isset($contactForm->message) ? $contactForm->message : ""; ?></textarea>
+                </div>
+
+                <div class="text-center">
+                    <input type="submit" class="bg-primary text-light rounded-sm" name="submit" id="submit" value="Submit">      
+                    <input type="reset" name="Reset" id="button" value="Clear Form">
+                </div>
+            </form>
+        </div>
+        
+    </body>
+
+</html>
